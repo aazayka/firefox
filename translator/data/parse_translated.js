ffb047500df24a2d9d914f45ddb1f65cafa0ac2f
aazayka <aazayka@mail.ru>
2014-11-15 21:41:28 +0300

    All worked

--- translator/data/parse_translated.js
+++ translator/data/parse_translated.js
@@ -1,21 +1,29 @@
 
 try {
 	var translated = document.querySelector('.b-translation__card')
-	if (translated == null) {
-	  // One variant
-	  var translationFull = document.querySelector('.b-translation__fulltext');
-	  if (translationFull == null) {
-		generatedHTML = "<h2>That's a shame</h2>";
-	  } else {
-		generatedHTML = document.querySelector('.b-translation__fulltext').innerHTML;
-	  }
-	} else {
+	if (translated != null) {
 	  // Many variants
 	  var translationGroups = document.querySelectorAll('.b-translation__group');
 	  for (var i = 0; i < translationGroups.length; ++i) {
 		generatedHTML = translationGroups[i].innerHTML;
-	  }  
+	  }
 	}
+	
+	// One variant
+	translated = document.querySelector('.b-translation__fulltext');
+	if (translated != null) {
+		generatedHTML = translated.innerHTML;
+	}
+	
+	translated = document.querySelector('.b-translation_type_example');
+	if (translated != null) {
+		generatedHTML = translated.innerHTML;
+	}
+		
+	if (generatedHTML == "") {
+		console.log("Still empty");
+		generatedHTML = "<h2>Нихрена не найдено</h2>"
+	} 
 } catch(e) {
 	generatedHTML = '<h2>Нихрена не найдено</h2>';
 }

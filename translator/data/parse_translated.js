dd92ca2130fe183ca1e97cbd092dd147fec6d664
aazayka <aazayka@mail.ru>
2014-11-15 21:33:27 +0300

    Попробую перегруппировать код

--- translator/data/parse_translated.js
+++ translator/data/parse_translated.js
@@ -1,22 +1,22 @@
+
 try {
-	var generatedHTML;
 	var translated = document.querySelector('.b-translation__card')
 	if (translated == null) {
 	  // One variant
 	  var translationFull = document.querySelector('.b-translation__fulltext');
 	  if (translationFull == null) {
-		console.log("<h2>That's a shame</h2>");
+		generatedHTML = "<h2>That's a shame</h2>";
 	  } else {
-		console.log(document.querySelector('.b-translation__fulltext').innerHTML);
+		generatedHTML = document.querySelector('.b-translation__fulltext').innerHTML;
 	  }
 	} else {
 	  // Many variants
 	  var translationGroups = document.querySelectorAll('.b-translation__group');
 	  for (var i = 0; i < translationGroups.length; ++i) {
-		console.log(i + " = " + translationGroups[i].innerHTML);
+		generatedHTML = translationGroups[i].innerHTML;
 	  }  
 	}
-	self.postMessage(generatedHTML);
 } catch(e) {
-	self.postMessage('<h2>Нихрена не найдено</h2>');
+	generatedHTML = '<h2>Нихрена не найдено</h2>';
 }
+self.postMessage(generatedHTML);
--- translator/lib/main.js
+++ translator/lib/main.js
@@ -9,16 +9,17 @@ let selection = require("sdk/selection");
 var panel = require("sdk/panel").Panel({
   width: 500,
   height: 400,
-  contentScript: "self.port.on('panelContent', function(data){document.body.innerHTML = data;});"
+  contentScript: "self.port.on('panelContent', function(data){document.body.innerHTML = data;});",
+  contentStyleFile: self.data.url("styles.css")
 });
 
-var translatedText; 
+var translatedText;
+var generatedHTML; 
 
 function ShowPanel(message) {
-        panel.port.emit('panelContent', '<h2 style="font-size:12pt;">' + translatedText + '</h2><span style="font-size: 10pt;' + message + '</span>');
+        panel.port.emit('panelContent', '<h2 style="font-size:12pt;">' + translatedText + '</h2><span style="font-size: 10pt;">' + message + '</span>');
         panel.show();
 }
-
 
 function Translate() {
 translatedText = selection.text;
@@ -27,7 +28,7 @@ if (translatedText) {
 	var pageWorkers = require("sdk/page-worker");
 	   pageWorkers.Page({
 		  contentURL: "http://slovari.yandex.ru/"+translatedText+"/en-ru/",
-		  contentScriptFile: self.data.url("parse_translated.js"),
+		  contentScriptFile: data.url("parse_translated.js"),
 		  contentScriptWhen: "ready",
 		  onMessage: ShowPanel
 		  });
